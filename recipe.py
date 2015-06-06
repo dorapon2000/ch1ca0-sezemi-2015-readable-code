9cda07e825ec519f721d9343557c1dbb745c8740
dorapon <dorapon2000@gmail.com>
2015-06-06 14:51:28 +0900

    spec5: 仕様5作成

--- recipe.py
+++ recipe.py
@@ -4,6 +4,6 @@ recipe_file = open('recipe-data.json', 'r')
 recipe_json = json.load(recipe_file)
 
 recipe_list = recipe_json.keys()
-for recipe in recipe_list:
-    print(recipe)
+for recipe_no, recipe in enumerate(recipe_list):
+    print(str(recipe_no) + ": " +recipe)
 
