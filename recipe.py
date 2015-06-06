c56b7cc2118e9c02e941f768447cc3b0e853caf0
dorapon <dorapon2000@gmail.com>
2015-06-06 14:25:50 +0900

    spec4: json形式の表示から、レシピを取り出してそれを表示にかえた

--- recipe.py
+++ recipe.py
@@ -3,4 +3,7 @@ import json
 recipe_file = open('recipe-data.json', 'r')
 recipe_json = json.load(recipe_file)
 
-print(json.dumps(recipe_json, indent = 4))
+recipe_list = recipe_json.keys()
+for recipe in recipe_list:
+    print(recipe)
+
