660597ff1b37ed8f39e7a6e21842a83576854736
dorapon <dorapon2000@gmail.com>
2015-06-06 15:24:05 +0900

    spec6: 50%

--- recipe.py
+++ recipe.py
@@ -1,9 +1,12 @@
 import json
+import sys
 
 recipe_file = open('recipe-data.json', 'r')
 recipe_json = json.load(recipe_file)
 
 recipe_list = recipe_json.keys()
 for recipe_no, recipe in enumerate(recipe_list):
-    print(str(recipe_no) + ": " +recipe)
-
+    if sys.argv.length() == 1:        
+        print(str(recipe_no) + ": " +recipe)
+    else:
+        show_recipe_no = sys.argv[1] #sys.argvの表示が2つになってる
